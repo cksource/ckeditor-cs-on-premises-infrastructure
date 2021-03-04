5c89119c865d1bf6775a30c4ed08c073cf94b6d6
LukaszGudel <gudel.lukasz@gmail.com>
2021-03-04 10:07:21 +0100

    Add editDockerComposeFile function

--- quick-start/setup.js
+++ quick-start/setup.js
@@ -4,12 +4,16 @@ const exec = util.promisify(require('child_process').exec);
 let LICENSE_KEY = '';
 let DOCKER_TOKEN = '';
 let ENV_SECRET = '';
+let CS_PORT = 8005;
+let NODE_PORT = 300;
 
 (async function onPremisesSetup() {
 
    getCredentials()
 
    await pullDockerImage()
+
+   editDockerComposeFile()
 
 })();
 
@@ -38,5 +42,30 @@ async function pullDockerImage() {
    } catch( err ) {
       //TODO: print clear instructions for users when token is incorrect
       console.log( err.stderr )
+   }
+}
+
+function editDockerComposeFile() {
+
+   const fs = require('fs');
+   const yaml = require('js-yaml');
+   
+   try {
+      // read file 
+      console.log('Editing docker-compose.yml file...')
+      let dockerComposeFile = fs.readFileSync('./docker-compose.yml', 'utf8');
+      let dockerComposeObject = yaml.load(dockerComposeFile);
+
+      // edit file 
+      dockerComposeObject.services['ckeditor-cs'].environment.LICENSE_KEY = LICENSE_KEY;
+      dockerComposeObject.services['ckeditor-cs'].ports[0] = `${ CS_PORT }:8000`;
+       
+      // save file
+      dockerComposeFile = yaml.dump(dockerComposeObject);
+      fs.writeFileSync('./docker-compose.yml', dockerComposeFile, 'utf8');
+      console.log('docker-compose.yml file edit done')
+
+   } catch ( err ) {
+       console.log( err );
    }
 }
