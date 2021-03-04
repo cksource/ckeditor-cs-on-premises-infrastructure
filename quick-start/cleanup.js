a6b40d924f712720e5d520bd53b27f94218d4d31
LukaszGudel <gudel.lukasz@gmail.com>
2021-03-04 08:53:37 +0100

    Init node project, create cleanup command

---
+++ quick-start/cleanup.js
@@ -0,0 +1,23 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function removeContainer( containerName ) {
+   try {
+      //const { stdout: out1, stderr: err1 } =
+      await exec( `docker stop ${ containerName }` )
+      
+      //const { stdout: out2, stderr: err2 } =  
+      await exec( `docker container rm ${ containerName }` )
+      
+      console.log( `${ containerName } container removed` )
+   }
+   catch(err) {
+      console.log(`There was an error while removing ${ containerName }`)
+      return
+   }
+   
+}
+
+removeContainer('quick-start-cs')
+removeContainer('quick-start-mysql')
+removeContainer('quick-start-redis')
