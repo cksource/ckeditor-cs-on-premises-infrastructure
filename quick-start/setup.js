5d6c9354771d42705a5e0dc02516bc195fa743b7
LukaszGudel <gudel.lukasz@gmail.com>
2021-03-05 14:41:42 +0100

    Add functions for terminal output

--- quick-start/setup.js
+++ quick-start/setup.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk');
+const ora = require('ora');
+
 let LICENSE_KEY = '';
 let DOCKER_TOKEN = '';
 let ENV_SECRET = '';
@@ -6,6 +9,9 @@ let NODE_PORT = 3000;
 let IP_ADDR = '';
 
 (async function onPremisesSetup() {
+
+   info( `   This is ${ chalk.green('On-Premises Quick-Start') } installation` )
+   info( `   Add more informations here\n\n` )
 
    getCredentials()
 
@@ -149,3 +155,16 @@ async function createEnvironment() {
 }
 
 
+
+function stepError( message ) {
+   console.log( chalk.bold.red( ' \u2718 '  + message ) )
+}
+function stepInfo( message ) {
+   console.log( chalk.bold.green( ' \u2713 ' ) + chalk.bold( message ) )
+}
+function stepSpinner( message ) {
+   return ora( chalk.bold( message ) )
+}
+function info( message ) {
+   console.log( chalk.bold( message ) )
+}
