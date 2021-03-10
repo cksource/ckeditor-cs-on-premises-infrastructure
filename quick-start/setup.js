3687d7583797e30a4715d5d049dc17370359fb3c
LukaszGudel <gudel.lukasz@gmail.com>
2021-03-10 08:17:09 +0100

    Move logging function to utils/logger, minor fixes

--- quick-start/setup.js
+++ quick-start/setup.js
@@ -1,7 +1,7 @@
 const util = require( 'util' );
 const exec = util.promisify( require( 'child_process' ).exec );
 const chalk = require( 'chalk' );
-const ora = require( 'ora' );
+const log = require('./utils/logger')
 
 let LICENSE_KEY = '';
 let DOCKER_TOKEN = '';
@@ -37,8 +37,8 @@ let IP_ADDR = '';
 
 function printWelcomeMessage() {
    console.clear();
-   info( `   This is ${ chalk.green('On-Premises Quick-Start') } installation` );
-   info( `   Add more informations here\n` );
+   log.info( `   This is ${ chalk.green('On-Premises Quick-Start') } installation` );
+   log.info( `   Add more informations here\n` );
 }
 
 async function getCredentials() {
@@ -59,7 +59,7 @@ async function getCredentials() {
    if ( !argv.env_secret ) properties.push( 'env_secret' );
 
    if ( properties.length > 0 ) {
-      info( 'Some credentials are missing or were passed incorrectly. Please provide them below. \n' );
+      log.info( 'Some credentials are missing or were passed incorrectly. Please provide them below. \n' );
    }
    prompt.start();
    result = await prompt.get( properties );
@@ -69,32 +69,32 @@ async function getCredentials() {
    ENV_SECRET = ENV_SECRET || result.env_secret;
 
    if ( properties.length > 0 ) {
-      info( '\n' );
+      log.info( '\n' );
    }
 }
 
 function validateCredentails() {
    const licenseKeyRegex = /^[0-9a-f]*$/;
    if ( LICENSE_KEY.length < 300 || !licenseKeyRegex.test( LICENSE_KEY ) ) {
-      stepError( 'Validating credentials' );
-      info( chalk.red( '\n Provided License Key is invalid \n' ) );
+      log.stepError( 'Validating credentials' );
+      log.error( '\n Provided License Key is invalid \n' );
       process.exit( 1 );
    }
 
    const dockerTokenRegex = /^[0-9a-f\-]*$/;
    if ( DOCKER_TOKEN.length != 36 || !dockerTokenRegex.test( DOCKER_TOKEN ) ) {
-      stepError( 'Validating credentials' );
-      info( chalk.red( '\n Provided Docker Token is invalid \n' ) );
+      log.stepError( 'Validating credentials' );
+      log.error( '\n Provided Docker Token is invalid \n' );
       process.exit( 1 );
    }
 
    if ( ENV_SECRET.length == 0 ) {
-      stepError( 'Validating credentials' );
-      info( chalk.red( '\n Environment secret can not be empty \n' ) );
+      log.stepError( 'Validating credentials' );
+      log.error( '\n Environment secret can not be empty \n' );
       process.exit( 1 );
    }
 
-   stepInfo( 'Validating credentials' );
+   log.stepInfo( 'Validating credentials' );
 }
 
 async function validateEnvironment() {
@@ -103,50 +103,50 @@ async function validateEnvironment() {
       const dockerVersion = parseFloat(dockerExec.stdout.split( ' ' )[2])
       
       if ( dockerVersion === NaN || dockerVersion < 18 ) {
-         stepError('Validating environment')
-         info( `\n Wrong docker version. Please install newer version of docker to run this setup process.` )
+         log.stepError('Validating environment')
+         log.error( `\n Wrong docker version. Please install newer version of docker to run this setup process.` )
          process.exit(1)
       } 
    } catch ( err ) {
-      stepError('Validating environment')
-      info( `\n Docker is not installed on your machine. Please install docker to run this setup process.` )
+      log.stepError('Validating environment')
+      log.error( `\n Docker is not installed on your machine. Please install docker to run this setup process.` )
       process.exit(1)
    }
 
-   stepInfo('Validating environment')
+   log.stepInfo('Validating environment')
 }
 
 async function loginToDockerRegistry() { 
-   const loginSpinner = new Spinner( 'Docker registry authorization...' );
+   const loginSpinner = new log.Spinner( 'Docker registry authorization...' );
    loginSpinner.start();
 
    try {
       await exec( `docker login -u cs -p ${ DOCKER_TOKEN } https://${ DOCKER_ENDPOINT }` )
 
       loginSpinner.stop();
-      stepInfo( 'Docker registry authorization' );
+      log.stepInfo( 'Docker registry authorization' );
    } catch( err ) {
       //TODO: print clear instructions for users when token is incorrect
       loginSpinner.stop();
-      stepError( 'Docker registry authorization\n' );
+      log.stepError( 'Docker registry authorization\n' );
       console.log( err.stderr );
       process.exit( 1 );
    }
 }
 
 async function pullDockerImage() {
-   const downloadSpinner = new Spinner( 'Pulling docker image...' );
+   const downloadSpinner = new log.Spinner( 'Pulling docker image...' );
    downloadSpinner.start();
 
    try {
       await exec( `docker pull ${ DOCKER_ENDPOINT }/cs:latest` )
 
       downloadSpinner.stop();
-      stepInfo( 'Pulling docker image' );
+      log.stepInfo( 'Pulling docker image' );
    } catch( err ) {
       //TODO: print clear instructions for users: Check your internet connection and try again?
       downloadSpinner.stop();
-      stepError( 'Pulling docker image\n' );
+      log.stepError( 'Pulling docker image\n' );
       console.log( err.stderr );
       process.exit( 1 );
    }
@@ -166,16 +166,16 @@ function editDockerComposeFile() {
        
       dockerComposeFile = yaml.dump( dockerComposeObject );
       fs.writeFileSync( './docker-compose.yml', dockerComposeFile, 'utf8' );
-      stepInfo( 'Editing docker-compose.yml file' );
+      log.stepInfo( 'Editing docker-compose.yml file' );
    } catch ( err ) {
-      stepError( 'Editing docker-compose.yml file\n' );
+      log.stepError( 'Editing docker-compose.yml file\n' );
       console.log( err.message );
       process.exit( 1 );
    }
 }
 
 async function startDockerContainers() {
-   const dockerSpinner = new Spinner( 'Starting docker containers...' );
+   const dockerSpinner = new log.Spinner( 'Starting docker containers...' );
    dockerSpinner.start();
 
    const spawn = require( 'child_process' ).spawn;
@@ -187,18 +187,18 @@ async function startDockerContainers() {
    });
 
    return new Promise( ( resolve, reject ) => {
-      var serversAvailabilityCheck = setInterval( () => {
+      const serversAvailabilityCheck = setInterval( () => {
          if ( dockerImages.output.includes( 'Server is listening on port 8000.' ) &&
                dockerImages.output.includes( 'Node-server is listening on port 3000' ) ) {
             clearInterval( serversAvailabilityCheck );
             dockerSpinner.stop();
-            stepInfo( 'Starting docker containers' );
+            log.stepInfo( 'Starting docker containers' );
             resolve();
          }
 
          if ( dockerImages.output.includes( 'Wrong license key.' ) ) {
             dockerSpinner.stop();
-            stepError( 'Starting docker containers' );
+            log.stepError( 'Starting docker containers' );
             process.exit( 1 );
          }
       }, 100); 
@@ -210,7 +210,7 @@ async function createEnvironment() {
    const interfaces = require( 'os' ).networkInterfaces();
    const machineIpAddresses = [];
 
-   const envSpinner = new Spinner( 'Creating environment...' );
+   const envSpinner = new log.Spinner( 'Creating environment...' );
    envSpinner.start();
 
    Object.keys( interfaces ).forEach( device => {
@@ -238,18 +238,18 @@ async function createEnvironment() {
    try {
       await axios.post( `http://localhost:${ NODE_PORT }/init`, body )
       envSpinner.stop();
-      stepInfo( 'Creating environment' );
+      log.stepInfo( 'Creating environment' );
    }
    catch ( err ) {
       envSpinner.stop();
-      stepError( 'Creating environment' );
+      log.stepError( 'Creating environment' );
       process.exit(1)
    }
 }
 
 function printInstructionsAfterInstallation() {
-   info( `${ chalk.green('\n   Installation complete') }` );
-   info( `   Visit ${ chalk.underline.cyan(`http://${ IP_ADDR }:${ NODE_PORT }`) } to start collaborating` );
+   log.info( `${ chalk.green('\n   Installation complete') }` );
+   log.info( `   Visit ${ chalk.underline.cyan(`http://${ IP_ADDR }:${ NODE_PORT }`) } to start collaborating` );
 }
 
 async function findFirstUnusedPort( port ) {
@@ -258,23 +258,7 @@ async function findFirstUnusedPort( port ) {
    
    if ( inUse ) {
       return findFirstUnusedPort( port + 1 )
-   } else {
-      return port
    }
+   return port
 }
 
-function stepError( message ) {
-   console.log( chalk.bold.red( '\u2718 '  + message ) );
-}
-
-function stepInfo( message ) {
-   console.log( chalk.bold.green( '\u2713 ' ) + chalk.bold( message ) );
-}
-
-function Spinner( message ) {
-   return ora( chalk.bold( message ) );
-}
-
-function info( message ) {
-   console.log( chalk.bold( message ) );
-}
---
+++ quick-start/utils/logger.js
@@ -0,0 +1,30 @@
+const chalk = require( 'chalk' );
+const ora = require( 'ora' );
+
+function info ( message ) {
+   console.log( chalk.bold( message ) );
+}
+
+function stepInfo ( message ) {
+   console.log( chalk.bold.green( '\u2713 ' ) + chalk.bold( message ) );
+}
+
+function error ( message ) {
+   console.log( chalk.bold.red( message ) );
+}
+
+function stepError ( message ) {
+   console.log( chalk.bold.red( '\u2718 '  + message ) );
+}
+
+function Spinner ( message ) {
+   return ora( chalk.bold( message ) );
+}
+
+module.exports = { 
+   info,
+   stepInfo,
+   error,
+   stepError,
+   Spinner
+}
