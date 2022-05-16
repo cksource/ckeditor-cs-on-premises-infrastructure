0b9245dd1731a6a302cc1bd8ac32ac7afe33c7ad
LukaszGudel <l.gudel@cksource.com>
2022-05-16 11:34:51 +0200

    Fix eslint in quick-start

--- quick-start/.eslintrc.js
+++ quick-start/.eslintrc.js
@@ -1,9 +1,7 @@
-require( '@cksource-cs/eslint-config-cs-module/eslint-patch-dependency-loader' );
-
 module.exports = {
-   root: true,
-   env: {
-      "browser": true
-   },
-   extends: '@cksource-cs/eslint-config-cs-module'
+	root: true,
+	env: {
+		'browser': true
+	},
+	extends: '@cksource-cs/eslint-config-cs-module'
 };
